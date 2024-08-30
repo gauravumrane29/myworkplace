592ce10f36328ab43643ab79b2221de0a2801a16
gauravumrane29 <46122182+gauravumrane29@users.noreply.github.com>
2024-08-30 18:40:35 +0530

    Create badges.js

---
+++ badges.js
@@ -0,0 +1,13 @@
+function toggleDetails(sectionId) {
+    // Hide all sections
+    const sections = document.querySelectorAll('.details');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    // Show the selected section
+    const selectedSection = document.getElementById(sectionId);
+    if (selectedSection) {
+        selectedSection.style.display = 'block';
+    }
+}
